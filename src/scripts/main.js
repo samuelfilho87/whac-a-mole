fb4844f115ec8b4fd747d38d9383243b0d933a20
Samuel Damasceno Filho <samuelfilho@outlook.com>
2023-09-29 20:32:19 -0300

    feat: time left

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,16 +2,31 @@ const state = {
     view: {
         squares: document.querySelectorAll('.square'),
         enemy: document.querySelector('.enemy'),
-        timeLeft: document.querySelector('#time-left .value-status'),
+        timeLeft: document.querySelector('#time .value-status'),
         score: document.querySelector('#score .value-status'),
     },
     values: {
-        timerId: null,
         gameVelocity: 1000,
         hitPosition: 0,
         score: 0,
+        timeLeft: 60,
     },
+    actions: {
+        randoSquareTimer: setInterval(randomSquare, 1000),
+        timeLeftTimer: setInterval(countDown, 1000),
+    }
 };
+
+function countDown() {
+    state.values.timeLeft--;
+    state.view.timeLeft.textContent = state.values.timeLeft;
+
+    if (state.values.timeLeft < 1) {
+        clearInterval(state.actions.timeLeftTimer);
+        clearInterval(state.actions.randoSquareTimer);
+        alert(`Game Over! O seu resultado foi: ${state.values.score}`);
+    }
+}
 
 function randomSquare() {
     state.view.squares.forEach(square => {
@@ -22,10 +37,6 @@ function randomSquare() {
     const randomSquare = state.view.squares[randomNumber];
     randomSquare.classList.add('enemy');
     state.values.hitPosition = randomSquare.id;
-}
-
-function moveEnemy() {
-    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
 }
 
 function addListenerHitBox() {
@@ -41,7 +52,6 @@ function addListenerHitBox() {
 }
 
 function execute() {
-    moveEnemy();
     addListenerHitBox();
 }
 
